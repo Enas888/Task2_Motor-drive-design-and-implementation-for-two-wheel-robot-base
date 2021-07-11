5069f12f8880c37ce50b78c15091f61251652bc8
Enas888 <86245745+Enas888@users.noreply.github.com>
2021-07-11 19:42:48 +0300

    Add files via upload

---
+++ task2_Motor drive design and implementation for two-wheel robot base code.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  
+   digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  
+  
+}
